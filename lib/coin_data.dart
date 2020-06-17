8ccbe03a79748980d97e1b0061ead02811821477
Dishank Jindal <iamprojectro@gmail.com>
2020-06-18 03:23:05 +0530

    done with the gold challenge

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,12 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const apiUrl = 'https://min-api.cryptocompare.com/data/pricemulti';
+const apiKey =
+    '0fc4379f875a8a0f3c9dc4b881e4259955b7becc60dd798a14519c9a54bb85e6';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +37,43 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future<Map<String, String>> getExchangeRate(
+      List<String> fromWhat, String intoWhat) async {
+    String parseFromWhat = '';
+    for (int i = 0; i < fromWhat.length; i++) {
+      if (i != 0) parseFromWhat += ',';
+      parseFromWhat += fromWhat[i];
+    }
+
+    http.Response response = await http
+        .get('$apiUrl?fsyms=$parseFromWhat&tsyms=$intoWhat&api_key=$apiKey');
+    String data = response.body;
+
+    Map<String, String> exchangeRate = {};
+    fromWhat.forEach((element) {
+      exchangeRate[element] =
+          '${jsonDecode(data)[element]['$intoWhat'].toDouble().toString()}';
+    });
+    return exchangeRate;
+  }
+
+  List<Text> getTextCurrencyOptions() {
+    List<Text> data = [];
+    currenciesList.forEach((e) {
+      data.add(Text(
+        e,
+        style: TextStyle(color: Colors.white),
+      ));
+    });
+    return data;
+  }
+
+  List<String> getCurrencyOptions() {
+    List<String> data = [];
+    currenciesList.forEach((e) {
+      data.add(e);
+    });
+    return data;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,11 +1,78 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'coin_data.dart';
+
+enum dataaa {
+  AUD,
+  BRL,
+  CAD,
+  CNY,
+  EUR,
+  GBP,
+  HKD,
+  IDR,
+  ILS,
+  INR,
+  JPY,
+  MXN,
+  NOK,
+  NZD,
+  PLN,
+  RON,
+  RUB,
+  SEK,
+  SGD,
+  USD,
+  ZAR
+}
+
+final int selectInitialScrollValue = dataaa.INR.index;
+final String setInitialCurrency = 'INR';
+
 class PriceScreen extends StatefulWidget {
+  final CoinData coinData = new CoinData();
+
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  Map<String, String> finalRate = {};
+  List<String> coins = ['BTC', 'ETH', 'LTC'];
+  String currency = setInitialCurrency;
+
+  Future<void> exchangeRate() async {
+    Map<String, String> exchangeRate =
+        await widget.coinData.getExchangeRate(coins, currency);
+    setState(() {
+      finalRate.addAll(exchangeRate);
+    });
+  }
+
+  CupertinoPicker cupertinoPicker() {
+    return CupertinoPicker(
+      onSelectedItemChanged: (int value) {
+        setState(() {
+          exchangeRate();
+          currency = widget.coinData.getCurrencyOptions()[value].toString();
+          finalRate = {};
+        });
+      },
+      itemExtent: 32.0,
+      children: widget.coinData.getTextCurrencyOptions(),
+      scrollController:
+          FixedExtentScrollController(initialItem: selectInitialScrollValue),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    exchangeRate();
+    currency = widget.coinData.getCurrencyOptions()[selectInitialScrollValue];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +80,7 @@ class _PriceScreenState extends State<PriceScreen> {
         title: Text('🤑 Coin Ticker'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Padding(
@@ -27,7 +94,9 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = ${finalRate['BTC'] == null
+                      ? 'FETCH'
+                      : finalRate['BTC']} $currency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -37,12 +106,64 @@ class _PriceScreenState extends State<PriceScreen> {
               ),
             ),
           ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ETH = ${finalRate['ETH'] == null
+                      ? 'FETCH'
+                      : finalRate['ETH']} $currency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 LTC = ${finalRate['LTC'] == null
+                      ? 'FETCH'
+                      : finalRate['LTC']} $currency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Row(
+            children: <Widget>[],
+          ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: cupertinoPicker(),
           ),
         ],
       ),
